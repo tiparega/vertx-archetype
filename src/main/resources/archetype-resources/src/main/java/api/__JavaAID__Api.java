bb4d07a9b72cc0fb502a467769f14e57a8d5c0f1
Alejandro Ladera <11555126+tiparega@users.noreply.github.com>
2021-10-23 01:38:04 +0200

    API Operations

--- src/main/resources/archetype-resources/src/main/java/api/__JavaAID__Api.java
+++ src/main/resources/archetype-resources/src/main/java/api/__JavaAID__Api.java
@@ -1,10 +1,17 @@
 package ${package}.api;
 
+import ${package}.config.Constants;
+
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Future;
+import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpServerResponse;
+import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
-import io.vertx.ext.web.handler.BodyHandler;
+import io.vertx.ext.web.openapi.RouterBuilder;
+import io.vertx.ext.web.openapi.RouterBuilderOptions;
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
@@ -15,19 +22,27 @@ public class ${JavaAID}Api {
 		this.vertx = vertx;
 	}
 
-	public Router getRouter() {
-		Router ${zJavaAIDVar}Api = Router.router(vertx);
-		${zJavaAIDVar}Api.post().handler(BodyHandler.create());
-		${zJavaAIDVar}Api.post("/changeThis").handler(this::action);
-
-		return ${zJavaAIDVar}Api;
+	public void getRouter(Handler<AsyncResult<Router>> handle) {
+		RouterBuilder.create(vertx, "api.yaml").onSuccess(routerBuilder -> {
+			routerBuilder.setOptions(new RouterBuilderOptions().setRequireSecurityHandlers(false)); // TODO: AUTH
+			routerBuilder.operation("getPetById").handler(this::action).failureHandler(this::error);
+			handle.handle(Future.succeededFuture(routerBuilder.createRouter()));
+		}).onFailure(err -> {
+			handle.handle(Future.failedFuture(err));
+		});
 	}
 
 	private void action(RoutingContext routingContext) {
-		int parameter = routingContext.getBodyAsJson().getInteger("parameter");
-		log.info("Returning " + parameter);
+		HttpServerResponse response = routingContext.response();
+		response.end(new JsonObject().put("petId", routingContext.pathParam("petId")).encode());
+	}
+
+	private void error(RoutingContext routingContext) {
+		log.info("Request error: " + routingContext.failure());
 
 		HttpServerResponse response = routingContext.response();
-		response.end(String.valueOf(parameter));
+		response.setStatusCode(Constants.HTTP_RESPONSE_BAD_REQUEST).putHeader("Content-Type", "text/html")
+				.end("<h1>" + Constants.HTTP_RESPONSE_BAD_REQUEST + " Bad request</h1><br />"
+						+ routingContext.failure().getLocalizedMessage());
 	}
 }
--- src/main/resources/archetype-resources/src/main/java/verticle/__JavaAID__Verticle.java
+++ src/main/resources/archetype-resources/src/main/java/verticle/__JavaAID__Verticle.java
@@ -6,6 +6,7 @@ import ${package}.api.${JavaAID}Api;
 import ${package}.config.ConfigUtil;
 import ${package}.config.Constants;
 import ${package}.config.HealthCheck;
+
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.binder.jvm.JvmMemoryMetrics;
 import io.micrometer.core.instrument.binder.jvm.JvmThreadMetrics;
@@ -36,30 +37,33 @@ public class ${JavaAID}Verticle extends AbstractVerticle {
 				HttpServer server = vertx.createHttpServer(options);
 		
 				Router router = Router.router(vertx);
-				
-				router.route("/api/*")
-					//.consumes("application/json")
-					.produces("application/json")
-					.subRouter(new ${JavaAID}Api(vertx).getRouter());
-				router.route("/alive").handler(HealthCheck.createLiveness(vertx));
-				router.route("/health").handler(HealthCheck.createReadyness(vertx));
-				router.route("/metrics").handler(PrometheusScrapingHandler.create());
-				
-				MeterRegistry registry = BackendRegistries.getDefaultNow();
-				new JvmMemoryMetrics().bindTo(registry);
-				new JvmThreadMetrics().bindTo(registry);
-				
-				router.route().handler(routingContext -> {
-					routingContext.fail(Constants.HTTP_RESPONSE_NOT_FOUND);
-				});
-		
-				server.requestHandler(router)
-					.exceptionHandler(e -> {
-						log.error("Unhandled error", e);
-					})
-				.listen(config.getInteger(Constants.PROP_SERVER_PORT), listening -> {
-					serverPort= server.actualPort();
-					log.info("Server started on port " + serverPort + " in " + (Instant.now().toEpochMilli() - start.toEpochMilli())  + " milliseconds");
+				new ${JavaAID}Api(vertx).getRouter(apiRouter -> {
+					if (apiRouter.succeeded()) {
+						router.route("/api/*")
+								// .consumes("application/json")
+								.produces("application/json").subRouter(apiRouter.result());
+						router.route("/alive").handler(HealthCheck.createLiveness(vertx));
+						router.route("/health").handler(HealthCheck.createReadyness(vertx));
+						router.route("/metrics").handler(PrometheusScrapingHandler.create());
+
+						MeterRegistry registry = BackendRegistries.getDefaultNow();
+						new JvmMemoryMetrics().bindTo(registry);
+						new JvmThreadMetrics().bindTo(registry);
+
+						router.route().handler(routingContext -> {
+							routingContext.fail(Constants.HTTP_RESPONSE_NOT_FOUND);
+						});
+
+						server.requestHandler(router).exceptionHandler(e -> {
+							log.error("Unhandled error", e);
+						}).listen(config.getInteger(Constants.PROP_SERVER_PORT), listening -> {
+							serverPort = server.actualPort();
+							log.info("Server started on port " + serverPort + " in "
+									+ (Instant.now().toEpochMilli() - start.toEpochMilli()) + " milliseconds");
+						});
+					} else {
+						log.error("Error configuring API", apiRouter.cause());
+					}
 				});
 			} else {
 				log.error("Error retrieving configuration", configHandler.cause());
